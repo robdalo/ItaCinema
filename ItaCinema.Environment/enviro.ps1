0d86d5d467bba0c10ff7fadd687f8f8e00645947
Rob Daley <robd@itaboi.co.uk>
2020-08-30 22:19:26 +0100

    Revised to include separate staging and live Azure resource templates.

--- ItaCinema.Environment/enviro.ps1
+++ ItaCinema.Environment/enviro.ps1
@@ -1,18 +1,19 @@
 param(
 	[string] $action,
-	[string] $subAction
+	[string] $subAction,
+	[string] $environment
 )
 
 if ($action -eq "azure" -and $subAction -eq "deploy") {
-	$resourceGroupName = "ItaCinema"
+	$resourceGroupName = "ItaCinema" + $environment
 	$location = "centralus"
-	$templateFile = "azure\resources.json"
+	$templateFile = "azure\" + $environment.ToLower() + "\resources.json"
 
 	New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 	New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile
 }
 
 elseif ($action -eq "azure" -and $subAction -eq "destroy") {
-	$resourceGroupName = "ItaCinema"
+	$resourceGroupName = "ItaCinema" + $environment
 	Remove-AzResourceGroup -Name $resourceGroupName -Force
 }
